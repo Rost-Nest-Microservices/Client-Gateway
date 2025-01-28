2eb2dce37ebf16115dff297a4ec45bf8d9e95da5
jpena <juan.pena@wmcontractor.com>
2025-01-27 19:33:41 -0500

    Connection between gateway and auth ms

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
 import { NatsModule } from './nats/nats.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NatsModule],
+  imports: [ProductsModule, OrdersModule, AuthModule, NatsModule],
 })
 export class AppModule {}
